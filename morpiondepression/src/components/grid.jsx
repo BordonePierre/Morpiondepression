210fec65824125a01fa2db71d337b83f29f7ac3e
nadjiallah-zouaoui <nadjiallah.zouaoui@gmail.com>
2026-03-12 14:05:45 +0100

    gridmaj

--- morpiondepression/src/components/grid.jsx
+++ morpiondepression/src/components/grid.jsx
@@ -1,6 +1,12 @@
 function Grid(){
     return (
-        <div className="grid grid-cols-3 border-4 border-black">
+        <div className="grid grid-cols-3 border-4 border-yellow">
+
+            <div className="bg-red-600">  </div>
+            <div className="bg-red-600">  </div>
+            <div className="bg-red-600">  </div>
+            <div className="bg-red-600">  </div>
+            <div className="bg-red-600">  </div>
 
         </div>
     )
