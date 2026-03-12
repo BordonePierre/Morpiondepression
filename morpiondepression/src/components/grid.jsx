51cbd7630ed58b710b4cb5b5a5a113e37817aceb
nadjiallah-zouaoui <nadjiallah.zouaoui@gmail.com>
2026-03-12 14:47:44 +0100

    tab

--- morpiondepression/src/components/grid.jsx
+++ morpiondepression/src/components/grid.jsx
@@ -1,17 +1,23 @@
-function Grid(){
-    return (
-        <div className="grid grid-cols-3 border-4 border-yellow">
+function Grid() {
+  return (
+    <div className="flex justify-center mt-20">
+      <div className="grid grid-cols-3 border-2 border-black">
 
-            <div className="bg-red-600">  </div>
-            <div className="bg-red-600">  </div>
-            <div className="bg-red-600">  </div>
-            <div className="bg-red-600">  </div>
-            <div className="bg-red-600">  </div>
+        <div className="w-24 h-24 border-2 border-black"></div>
+        <div className="w-24 h-24 border-2 border-black"></div>
+        <div className="w-24 h-24 border-2 border-black"></div>
 
-        </div>
-    )
-       
-    
+        <div className="w-24 h-24 border-2 border-black"></div>
+        <div className="w-24 h-24 border-2 border-black"></div>
+        <div className="w-24 h-24 border-2 border-black"></div>
+
+        <div className="w-24 h-24 border-2 border-black"></div>
+        <div className="w-24 h-24 border-2 border-black"></div>
+        <div className="w-24 h-24 border-2 border-black"></div>
+
+      </div>
+    </div>
+  );
 }
 
 export default Grid;
