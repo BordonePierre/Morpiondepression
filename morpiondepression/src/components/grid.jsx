38c0ce57846360f1477b0676e69b54c71b81aec1
nadjiallah-zouaoui <nadjiallah.zouaoui@gmail.com>
2026-03-17 14:35:03 +0100

    grid

--- morpiondepression/src/components/grid.jsx
+++ morpiondepression/src/components/grid.jsx
@@ -1,22 +1,42 @@
+import { useState } from 'react';
+
 function Grid() {
+
+
+  const [texteBouton, setTexteBouton] = useState("");
+
+  function buttonClick() {
+    setTexteBouton("❌");
+  }
+
   return (
-    <div className="flex justify-center mt-20">
+
+    <div className="flex flex-col items-center">
+
+      <h1 className="font-bold text-[34px] mt-20">MORPION</h1>
+
+      <div className="flex justify-center mt-20">
+
       <div className="grid grid-cols-3 border-2 border-black">
 
-        <div className="w-24 h-24 border-2 border-black"></div>
-        <div className="w-24 h-24 border-2 border-black"></div>
-        <div className="w-24 h-24 border-2 border-black"></div>
 
-        <div className="w-24 h-24 border-2 border-black"></div>
-        <div className="w-24 h-24 border-2 border-black"></div>
-        <div className="w-24 h-24 border-2 border-black"></div>
+        <button className="w-24 h-24 border-2 border-black cursor-pointer hover:bg-amber-300" onClick={buttonClick}>{texteBouton}</button>
+        <button className="w-24 h-24 border-2 border-black cursor-pointer hover:bg-amber-300"></button>
+        <button className="w-24 h-24 border-2 border-black cursor-pointer hover:bg-amber-300"></button>
 
-        <div className="w-24 h-24 border-2 border-black"></div>
-        <div className="w-24 h-24 border-2 border-black"></div>
-        <div className="w-24 h-24 border-2 border-black"></div>
+        <button className="w-24 h-24 border-2 border-black cursor-pointer hover:bg-amber-300"></button>
+        <button className="w-24 h-24 border-2 border-black cursor-pointer hover:bg-amber-300"></button>
+        <button className="w-24 h-24 border-2 border-black cursor-pointer hover:bg-amber-300"></button>
+
+        <button className="w-24 h-24 border-2 border-black cursor-pointer hover:bg-amber-300"></button>
+        <button className="w-24 h-24 border-2 border-black cursor-pointer hover:bg-amber-300"></button>
+        <button className="w-24 h-24 border-2 border-black cursor-pointer hover:bg-amber-300"></button>
+
+        
 
       </div>
     </div>
+  </div>
   );
 }
 
